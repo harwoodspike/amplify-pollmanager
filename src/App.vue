ade9d7c94cf952956e5749eacad3548ac39f949a
Joshua Harwood <jharwood@harwoodspike.com>
2024-07-14 16:56:13 -0600

    Amplify setup changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import '@aws-amplify/ui-vue/styles.css'
-
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
-
-Amplify.configure(awsconfig);
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -10,6 +11,10 @@ export default defineConfig({
         find: './runtimeConfig',
         replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
       },
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
     ]
   }
 })
